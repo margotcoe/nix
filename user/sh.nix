b6aeee02365cc132d9d5935f8984b198ae272496
margotcoe <margot.coe@gmail.com>
2024-08-08 19:50:36 -0700

    tweaking

--- user/sh.nix
+++ user/sh.nix
@@ -1,15 +1,12 @@
 { config, pkgs, ... }:
 let
-
   myAliases = {
     la = "ls -la";
     dotdot = "cd ~/.dotfiles";
-
   };
 in {
   programs.zsh = {
     enable = true;
-    autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
     enableCompletion = true;
     shellAliases = myAliases;
@@ -18,15 +15,17 @@ in {
        %F{green}→%f "
       RPROMPT="%F{red}▂%f%F{yellow}▄%f%F{green}▆%f%F{cyan}█%f%F{blue}▆%f%F{magenta}▄%f%F{white}▂%f"
       [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
+
+      # Load zsh-autosuggestions
+      source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
     '';
   };
 
   home.packages = with pkgs; [
     bottom
     tree
-
+    zsh-autosuggestions  # <-- This adds zsh-autosuggestions to your environment
   ];
 
   programs.nix-index.enableZshIntegration = true;
-
 }
