575bd5234311ba6f4ae107acac807b7f847f0353
margotcoe <margot.coe@gmail.com>
2024-08-08 19:57:14 -0700

    changed hostname

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
        pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
-      hollyOS = lib.nixosSystem {
+      hallelujah = lib.nixosSystem {
 	inherit system;
 	modules = [ ./configuration.nix ];
 	};
