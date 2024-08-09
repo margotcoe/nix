adf46e377de57ed6d5bce94780f604fc1a1caa78
margotcoe <margot.coe@gmail.com>
2024-08-08 20:05:54 -0700

    changed colors

--- user/sh.nix
+++ user/sh.nix
@@ -11,24 +11,22 @@ in {
     enableCompletion = true;
     shellAliases = myAliases;
     initExtra = ''
-    autoload -Uz vcs_info
-    precmd() { vcs_info }
-    zstyle ':vcs_info:git:*' formats '%F{blue}[%b]%f'
+      autoload -Uz vcs_info
+      precmd() { vcs_info }
+      zstyle ':vcs_info:git:*' formats '%F{blue}[%b]%f'
 
-    PROMPT="◉ %F{pink}%n%f@%F{cyan}%m%f:%F{magenta}%~%f ${vcs_info_msg_0_} %F{lightgreen}→%f "
-    RPROMPT="%F{lightyellow}%D{%H:%M:%S}%f"
-    [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '    
-  
-     # Load zsh-autosuggestions
+      PROMPT="◉ %F{pink}%n%f@%F{cyan}%m%f:%F{magenta}%~%f ${vcs_info_msg_0_} %F{lightgreen}→%f "
+      RPROMPT="%F{lightyellow}%D{%H:%M:%S}%f"
+      [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
+
       source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-
     '';
   };
 
   home.packages = with pkgs; [
     bottom
     tree
-    zsh-autosuggestions  # <-- This adds zsh-autosuggestions to your environment
+    zsh-autosuggestions
   ];
 
   programs.nix-index.enableZshIntegration = true;
