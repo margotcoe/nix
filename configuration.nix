72675da7fa79f42de8eb2501c9707acb345a8acf
margotcoe <margot.coe@gmail.com>
2024-08-08 19:13:40 -0700

    added sh.nix

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./user/sh.nix
     ];
 
   # Bootloader.
---
+++ user/sh.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+let
+  
+  myAliases = {
+    la = "ls -la";
+    dotdot = "cd ~/.dotfiles";
+ 
+   };
+  in
+  {
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;
+    enableCompletion = true;
+    shellAliases = myAliases;
+    initExtra = ''
+    PROMPT=" ◉ %U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
+     %F{green}→%f "
+    RPROMPT="%F{red}▂%f%F{yellow}▄%f%F{green}▆%f%F{cyan}█%f%F{blue}▆%f%F{magenta}▄%f%F{white}▂%f"
+    [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
+    '';
+  };
+
+  home.packages = with pkgs; [
+    bottom tree
+   
+   ];
+
+  programs.nix-index.enableZshIntegration = true;
+
+  }
