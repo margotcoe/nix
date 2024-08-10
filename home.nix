a4571909474fb103c30a01f7763606ac8f8b70a8
margotcoe <margot.coe@gmail.com>
2024-08-09 19:32:42 -0700

    added editorconfig to home.nix

--- home.nix
+++ home.nix
@@ -22,6 +22,19 @@
     # '')
   ];
 
+  editorconfig = {
+    enable = true;
+    settings = {
+	charset = "utf-8";
+	end_of_line = "lf";
+	traim_trailing_whitespace = true;
+	insert_final_newline = true;
+	max_line_width = 78;
+	indent_style = "space";
+	indent_size = 2;
+    };
+  };
+
   home.file = {
     # Uncomment and add more files if needed
     # ".screenrc".source = ./dotfiles/screenrc;
