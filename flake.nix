daa21939265bb49384c2ec35314edfad6861192c
margotcoe <margot.coe@gmail.com>
2024-09-05 19:11:56 -0700

    server.nix

--- flake.nix
+++ flake.nix
@@ -1,38 +1,36 @@
 {
-
   description = "My First Flake";
 
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-      home-manager.url = "github:nix-community/home-manager/release-24.05";
-      home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs = { self, nixpkgs, home-manager, ... }:
     let
-       lib = nixpkgs.lib;
-       system = "x86_64-linux";
-       pkgs = nixpkgs.legacyPackages.${system};
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
-    nixosConfigurations = {
-      hallelujah = lib.nixosSystem {
-     	  inherit system;
-	      modules = [
-          ./configuration.nix
-          ./server.nix
-        ];
-        configuration = {
-          media_server.enable = true;
+      nixosConfigurations = {
+        hallelujah = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            ./media.nix  # Include your media module here
+          ];
+          # This should be removed as it's causing the error
+           configuration = {
+             media_server.enable = true;  # Enable media server services
+           };
         };
-    	};
-     };
-     homeConfigurations = {
-      margot = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-	modules = [ ./home.nix ];
-	};
-     };
-  };
-
-
+      };
+      homeConfigurations = {
+        margot = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
 }
