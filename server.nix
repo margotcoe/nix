fc6296b5bdace87f4ec28323f370dda5f4195957
margotcoe <margot.coe@gmail.com>
2024-09-05 19:01:52 -0700

    server.nix

--- server.nix
+++ server.nix
@@ -1,85 +1,51 @@
 { pkgs, config, lib, ... }:
-let
-  mediaDir = "/mnt/docker_storage";  # Matches your existing file structure
-  mediaGroup = "media";
-in
+
 {
   options = {
-    server.enable = lib.mkEnableOption "Serve and store media from this machine";
+    media_server.enable = lib.mkEnableOption "Enable media server services";
   };
 
   config = lib.mkIf config.media_server.enable {
-    # Create the media group
+    # Set up the media group
     users.groups.media = {};
 
-    # Create directories and set permissions to match your structure
-    systemd.tmpfiles.rules = [
-      "d ${mediaDir} 0775 root media -"
-      "d ${mediaDir}/complete 0775 root media -"
-      "d ${mediaDir}/complete/tv 0775 root media -"
-      "d ${mediaDir}/complete/movies 0775 root media -"
-      "d ${mediaDir}/downloads 0775 root media -"
-      "d ${mediaDir}/downloads/complete 0775 root media -"
-      "d ${mediaDir}/downloads/incomplete 0775 root media -"
-      "d ${mediaDir}/config 0775 root media -"
-    ];
-
-    # SABnzbd configuration
-    services.sabnzbd = {
-      enable = true;
-      package = pkgs.sabnzbd;
-   #   configFile = "${mediaDir}/config/sabnzbd/sabnzbd.ini";
-    };
-
-    # Sonarr configuration
-    services.sonarr = {
-      enable = true;
-      dataDir = "${mediaDir}/config/sonarr";
-      # mediaDirs = [ "${mediaDir}/complete/tv" ]; # Uncomment if you need specific media directories
-      # downloadDirs = [ "${mediaDir}/downloads" ]; # Uncomment if you need specific download directories
-      openFirewall = true;
-      group = mediaGroup;
-    };
-
-    # Radarr configuration
+    # Define Radarr service
     services.radarr = {
-      enable = true;
-      dataDir = "${mediaDir}/config/radarr";
-      # mediaDirs = [ "${mediaDir}/complete/movies" ]; # Uncomment if you need specific media directories
-      # downloadDirs = [ "${mediaDir}/downloads" ]; # Uncomment if you need specific download directories
-      openFirewall = true;
-      group = mediaGroup;
+      enable = true; # Enables the Radarr service
+      user = "radarr"; # Sets the user under which Radarr runs
+      package = pkgs.radarr; # Specifies the package to use
+      openFirewall = true; # Opens firewall rules for Radarr
+      group = "media"; # Sets the group for Radarr
+      dataDir = "/mnt/docker_storage/config/radarr"; # Directory for Radarr data
     };
 
-    # Plex configuration
-    services.plex = {
-      enable = true;
-      dataDir = "${mediaDir}/config/plex/db";
-      # mediaDirs = [ "${mediaDir}/complete" ]; # For both TV and movies, uncomment if needed
-      # transcodeDir = "/dev/shm"; # RAM-based transcoding, uncomment if needed
-      openFirewall = true;
-      group = mediaGroup;
+    # Define Sonarr service
+    services.sonarr = {
+      enable = true; # Enables the Sonarr service
+      user = "sonarr"; # Sets the user under which Sonarr runs
+      package = pkgs.sonarr; # Specifies the package to use
+      openFirewall = true; # Opens firewall rules for Sonarr
+      group = "media"; # Sets the group for Sonarr
+      dataDir = "/mnt/docker_storage/config/sonarr"; # Directory for Sonarr data
     };
 
-    # Bazarr configuration
+    # Define Bazarr service
     services.bazarr = {
-      enable = true;
-    #  dataDir = "${mediaDir}/config/bazarr";
-    #  mediaDirs = [
-    #    "${mediaDir}/complete/movies"
-    #    "${mediaDir}/complete/tv"
-    #  ];
-      openFirewall = true;
-      group = mediaGroup;
+      enable = true; # Enables the Bazarr service
+      user = "bazarr"; # Sets the user under which Bazarr runs
+      openFirewall = true; # Opens firewall rules for Bazarr
+      listenPort = 6767; # Port Bazarr listens on
+      group = "media"; # Sets the group for Bazarr
     };
 
-    # Set the environment system packages (optional)
-    environment.systemPackages = [
-      pkgs.sabnzbd
-      pkgs.plex
-      pkgs.sonarr
-      pkgs.radarr
-      pkgs.bazarr
-    ];
+    # Define SABnzbd service
+    services.sabnzbd = {
+      enable = true; # Enables the SABnzbd service
+      user = "sabnzbd"; # Sets the user under which SABnzbd runs
+      package = pkgs.sabnzbd; # Specifies the package to use
+      openFirewall = true; # Opens firewall rules for SABnzbd
+      group = "media"; # Sets the group for SABnzbd
+      configFile = "/mnt/docker_storage/config/sabnzbd/sabnzbd.ini"; # Path to the configuration file
+    };
   };
 }
