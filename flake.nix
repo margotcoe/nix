a94a1dfb2d703710d048b08ca8c5aac90324dc84
margotcoe <margot.coe@gmail.com>
2024-09-08 19:03:29 -0700

    testing

--- flake.nix
+++ flake.nix
@@ -17,11 +17,11 @@
           inherit system;
           modules = [
             ./configuration.nix
-            ./media.nix
+          #  ./media.nix
             {
               # Add your configuration options here
-              media_server.enable = true;
-              nixpkgs.config.allowUnfree = true;
+           #   media_server.enable = true;
+           #   nixpkgs.config.allowUnfree = true;
             }
           ];
           specialArgs = {
