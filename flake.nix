e037d209c3dac3b49925f531f352a754b9dd2cc4
margotcoe <margot.coe@gmail.com>
2024-09-08 20:03:08 -0700

    testing

--- flake.nix
+++ flake.nix
@@ -7,27 +7,24 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
-    let
-      lib = nixpkgs.lib;
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      nixosConfigurations = {
-        Media = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [ ./configuration.nix ./media.nix ];
-          specialArgs = { inherit pkgs; };
-#          configuration = { media_server.enable = true; };
-        };
-        config.allowUnfree = true;
-      };
-      homeConfigurations = {
-        margot = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ ./home.nix ];
-
-        };
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: let
+    system = "x86_64-linux"; # Define your system type
+    pkgs = nixpkgs.legacyPackages.${system};
+  in rec {
+    nixosConfigurations = {
+      Media = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ./media.nix ];
+        specialArgs = { inherit pkgs; };
+        configuration = { media_server.enable = true; };
       };
     };
+
+    homeConfigurations = {
+      margot = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+  };
 }
