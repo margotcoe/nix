ccb63c04f921980104a7c18247e0dd8629587328
margotcoe <margot.coe@gmail.com>
2024-09-08 19:42:36 -0700

    testing

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
           inherit system;
           modules = [ ./configuration.nix ./media.nix ];
           specialArgs = { inherit pkgs; };
-  #        configuration = { media_server.enable = true; };
+#          configuration = { media_server.enable = true; };
         };
       };
       homeConfigurations = {
